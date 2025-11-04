6f1d305ddb6aa71e9c49f1449d63cd095944bf15
imVarad27 <varadghatage7@gmail.com>
2025-11-04 08:23:18 +0530

    Code refactore

--- src/main/java/src/ManagementSystem/Domain/ValueObject/AssetPhysicalId.java
+++ src/main/java/src/ManagementSystem/Domain/ValueObject/AssetPhysicalId.java
@@ -1,9 +1,4 @@
 package src.ManagementSystem.Domain.ValueObject;
 
-import jakarta.validation.constraints.NotNull;
-
 public record AssetPhysicalId(String id) {
-    public String Id(){
-      return id;
-    }
 }
