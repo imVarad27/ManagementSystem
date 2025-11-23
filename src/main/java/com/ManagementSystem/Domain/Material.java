5c0920ca9e9781ada6394164be60816c608384ef
imVarad27 <varadghatage7@gmail.com>
2025-11-23 19:48:29 +0530

    Added readDto model for material

--- src/main/java/com/ManagementSystem/Domain/Material.java
+++ src/main/java/com/ManagementSystem/Domain/Material.java
@@ -2,13 +2,24 @@ package com.ManagementSystem.Domain;
 
 import com.ManagementSystem.Domain.Converter.PropertyListConverter;
 import com.ManagementSystem.Domain.ValueObject.Property;
-import jakarta.persistence.Convert;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Embedded;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Entity
+@Table(name = "materials")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Material {
+
+    @Embedded
+    @Id
+    @Column(name = "material_id", nullable = false, unique = true, updatable = false)
+    private String materialId;
 
     @Embedded
     private String name;
@@ -23,5 +34,6 @@ public class Material {
     private List<Property> properties;
 
     @Embedded
+    @Column(name = "parent_material_number")
     private String parentMaterialNumber;
 }
